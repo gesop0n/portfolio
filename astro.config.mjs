4866f6dc79aa287ea24d43c84441ce6eefab788b
gesop0n <ishikuro6.2@gmail.com>
2026-09-17 00:17:11 +0900

    update github name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Kota-35.github.io',
+  site: 'https://gesop0n.github.io',
   base: '/portfolio',
   vite: {
     plugins: [tailwindcss()],
